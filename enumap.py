73c11eb96c54ae29f34abc9fa2dcec9fa5fd90e1
Tad Leonard <tadfleonard@gmail.com>
2017-06-05 00:26:04 -0700

    redefined behavior for sparse, casted, missing values
    SparseEnumap now allows for default values to be set. If a sparse, casted collection is produced,
    missing values will be set to the default value. The default mapping itself has defaults,
    so missing values in casted collections with no default will be None.

--- enumap.py
+++ enumap.py
@@ -3,20 +3,20 @@ from enum import Enum
 from itertools import zip_longest
 
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 
 class Enumap(Enum):
-    """An Enum with convenience methods for mapping
-    ordered values to dictionaries and namedtuples whose
-    keys/fields are enforced to match the names of the Enum members"""
+    """An Enum that maps data to its ordered, named members.
+    Produces OrderedDicts and namedtuples while ensuring that the
+    keys/fields match the names of the Enum members."""
 
     @classmethod
     def names(cls):
-        names = getattr(cls, "__names", None)
-        if not names:
-            names = tuple(cls.__members__)
-            setattr(cls, "__names", names)
+        try:
+            names = cls.__names
+        except AttributeError:
+            names = cls.__names = tuple(cls.__members__)
         return names
 
     @classmethod
@@ -28,8 +28,11 @@ class Enumap(Enum):
         >>> Fruit.map("heart-shaped", "spherical", papaya="ellipsoid")
         OrderedDict(('apple', 'heart-shaped'), ('orange', ...), ...)
         """
-        mapping = cls._make_checked_mapping(*values, **named_values)
-        return OrderedDict(mapping)
+        try:
+            return cls.tuple_class()(*values, **named_values)._asdict()
+        except TypeError:
+            mapping = cls._make_checked_mapping(*values, **named_values)
+            return OrderedDict(((k, mapping[k]) for k in cls.names()))
 
     @classmethod
     def map_casted(cls, *values, **named_values):
@@ -45,6 +48,7 @@ class Enumap(Enum):
         OrderedDict(('index', 342), ('cost', Decimal("3242.23")), ...)
         """
         mapping = cls._make_casted_mapping(*values, **named_values)
+        return OrderedDict(((k, mapping[k]) for k in cls.names()))
         return OrderedDict(mapping)
 
     @classmethod
@@ -68,17 +72,18 @@ class Enumap(Enum):
         """Like `tuple`, but values are converted with the `types`
         mapping. Useful for deserializing ordered and named values."""
         mapping = cls._make_casted_mapping(*values, **named_values)
-        return cls.tuple_class()(**dict(mapping))
+        return cls.tuple_class()(**mapping)
 
     @classmethod
     def tuple_class(cls):
         """`namedtuple` class with fields that match this Enum's
         members and their ordering"""
-        t_type = getattr(cls, "__tuple_class", None)
-        if not t_type:
-            t_type = namedtuple(cls.__name__ + "_tuple", cls.names())
-            setattr(cls, "__tuple_class", t_type)
-        return t_type
+        try:
+            tuple_class = cls.__tuple_class
+        except AttributeError:
+            tuple_class = namedtuple(cls.__name__ + "_tuple", cls.names())
+            cls.__tuple_class = tuple_class
+        return tuple_class
 
     @classmethod
     def set_types(cls, *types, **named_types):
@@ -90,15 +95,20 @@ class Enumap(Enum):
         {'croissant': int, 'donut': float, 'muffin': int}
         """
         mapping = cls._make_checked_mapping(*types, **named_types)
-        setattr(cls, "__member_types", mapping)
+        cls.__member_types = mapping
 
     @classmethod
     def types(cls):
-        """Mapping like `{member_name: callable}` for `map/tuple_casted`"""
+        """Mapping like `{member_name: callable}` for `map/tuple_casted`.
+        This can either come from type annotations or `set_types`."""
         try:
-            return getattr(cls, "__member_types", None) or cls.__annotations__
+            types = cls.__member_types
         except AttributeError:
-            raise TypeError("{cls} has no types or type annotations")
+            if hasattr(cls, "__annotations__"):
+                types = cls.__member_types = cls.__annotations__
+            else:
+                raise TypeError("{cls} has no types or type annotations")
+        return types
 
     @classmethod
     def _make_checked_mapping(cls, *values, **named_values):
@@ -106,7 +116,7 @@ class Enumap(Enum):
         of the members of this Enum. Raises `KeyError` for both
         missing and invalid keys."""
         names = cls.names()
-        mapping = {**dict(zip(names, values)), **named_values}
+        mapping = dict(zip(names, values), **named_values)
         if set(mapping) == set(names):
             return mapping
         else:
@@ -121,13 +131,28 @@ class Enumap(Enum):
         on the `types()` mapping"""
         mapping = cls._make_checked_mapping(*values, **named_values)
         types = cls.types()
-        for name, value in mapping.items():
-            yield name, types[name](value)
+        return {k: types[k](v) for k, v in mapping.items()}
+
+
+_FILL = object()  # sentinal for missing values in sparse collections
 
 
 class SparseEnumap(Enumap):
-    """A less strict Enumap that provides `None` default values
-    for unspecified keys"""
+    """A less strict Enumap that provides default values
+    for unspecified keys. Invalid keys are still prohibited."""
+
+    @classmethod
+    def set_defaults(cls, *values, **named_values):
+        mapping = cls._make_checked_mapping(*values, **named_values)
+        cls.__member_defaults = mapping
+
+    @classmethod
+    def defaults(cls):
+        try:
+            return cls.__member_defaults
+        except AttributeError:
+            cls.__member_defaults = {k: None for k in cls.names()}
+            return cls.__member_defaults
 
     @classmethod
     def _make_checked_mapping(cls, *values, **named_values):
@@ -135,10 +160,24 @@ class SparseEnumap(Enumap):
         of the members of this Enum. Raises `KeyError` for both
         missing and invalid keys."""
         names = cls.names()
-        mapping = {**dict(zip_longest(names, values)), **named_values}
+        defaults = cls.defaults()
+        pairs = zip_longest(names, values, fillvalue=_FILL)
+        mapping = dict(pairs, **named_values)
         if set(mapping) == set(names):
-            return mapping
+            return {k: (v if v is not _FILL else defaults[k])
+                    for k, v in mapping.items()}
         else:
             invalid = set(mapping) - set(names)
             raise KeyError(f"{cls.__name__} requires keys {names}; "
                            f"invalid keys {invalid}")
+
+    @classmethod
+    def _make_casted_mapping(cls, *values, fillvalue=None, **named_values):
+        """Like `_make_checked_mapping`, but values are casted based
+        on the `types()` mapping"""
+        # note that the `fillvalue` keyword-only arg is ignored
+        mapping = cls._make_checked_mapping(*values, **named_values)
+        types = cls.types()
+        defaults = cls.defaults()
+        return {k: (types[k](v) if v != defaults[k] else v)
+                for k, v in mapping.items()}
